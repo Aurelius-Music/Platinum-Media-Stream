6dfce8b2b0436086b9409bcc1c79afc07b494cad
Aurelius-Music <aureliusmg9@gmail.com>
2026-09-14 10:39:05 -0400

    Update LiveRoom.jsx

--- src/LiveRoom.jsx
+++ src/LiveRoom.jsx
@@ -186,8 +186,8 @@ export default function LiveRoom() {
       }}
     >
       {isHost && <BroadcastControls roomName={roomName.trim()} />}
-      <VideoConference chatMessageFormatter={formatChatMessageLinks} />
-    </LiveKitRoom>
+<VideoConference chatMessageFormatter={formatChatMessageLinks} />
+</LiveKitRoom>
   );
 }
 
