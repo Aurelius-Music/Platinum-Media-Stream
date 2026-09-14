829a62e130262ea09ee00d87702ace90d261e695
Aurelius-Music <aureliusmg9@gmail.com>
2026-09-14 10:53:07 -0400

    Remove duplicate VideoConference component

--- src/LiveRoom.jsx
+++ src/LiveRoom.jsx
@@ -186,7 +186,7 @@ export default function LiveRoom() {
       }}
     >
       {isHost && <BroadcastControls roomName={roomName.trim()} />}
-<VideoConference chatMessageFormatter={formatChatMessageLinks} /><VideoConference ... /><VideoConference ... />
+<VideoConference chatMessageFormatter={formatChatMessageLinks} /><VideoConference ... />
 </LiveKitRoom>
   );
 }
